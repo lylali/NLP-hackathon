5e844923f6999cb8993f6242137b629451fddd0b
Joseph <perryjd@cardiff.ac.uk>
2025-06-10 14:32:01 +0100

    Bug fix

--- text_analysis.py
+++ text_analysis.py
@@ -1,4 +1,4 @@
-from transformers import pipeline, classifier
+from transformers import pipeline
 classifier = pipeline('text-classification', model='bhadresh-savani/bert-base-uncased-emotion', return_all_scores=True)
 prediction = classifier("I love using transformers. The best part is wide range of support and its easy to use", )
 print(prediction)
