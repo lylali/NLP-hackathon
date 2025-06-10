47b015015c69daa578161126c360b2b8ad5bf45d
Joseph <perryjd@cardiff.ac.uk>
2025-06-10 14:25:25 +0100

    Added initial Emotion/Politics script

---
+++ text_analysis.py
@@ -0,0 +1,8 @@
+from transformers import pipeline, classifier
+classifier = pipeline('text-classification', model='bhadresh-savani/bert-base-uncased-emotion', return_all_scores=True)
+prediction = classifier("I love using transformers. The best part is wide range of support and its easy to use", )
+print(prediction)
+
+classifier = pipeline('text-classification', model='harshal-11/Bert-political-classification', return_all_scores=True)
+prediction = classifier("I love using transformers. The best part is wide range of support and its easy to use", )
+print(prediction)
