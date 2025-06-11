526338eb96072e73c3621489b7136164e4c7b0e7
Joseph <perryjd@cardiff.ac.uk>
2025-06-11 12:38:46 +0100

    Added emotion analysis and basic graphical interface

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+import os
 import sys
 import time
 import requests
 import json
 import nltk
-import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 from tqdm import tqdm
 
 import torch
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from deep_translator import GoogleTranslator
+from bs4 import BeautifulSoup
 
 
 ### setup
@@ -25,7 +30,8 @@ SUMMARY_MIN = 8 # use dependent
 
 # ASIMILATE API
 API_URL = 'https://zfgp45ih7i.execute-api.eu-west-1.amazonaws.com/sandbox/api/search'
-API_KEY = sys.argv[1]
+with open('ASIMILATE.key', 'r') as f:
+	API_KEY = f.readline().strip()
 
 
 ### functions
@@ -98,3 +104,66 @@ def summarize_txt(text, tokenizer, model):
 	text_chunks = split_txt(text, tokenizer)
 	summary = summarize_txt_chunks(text_chunks, tokenizer, model)
 	return summary
+
+# type: (str, str) -> str
+def normalise_language(text, target='en'):
+	try:
+		return GoogleTranslator(source='auto', target=target).translate(text)
+	except Exception as e:
+		return f"[ERROR]: Translation failed"
+
+# type: () ->
+def url_2_txt(url):
+	try:
+		response = requests.get(url)
+		soup = BeautifulSoup(response.content, 'html.parser')
+		text = " ".join([p.get_text(strip=True) for p in soup.find_all('p')])
+		return text
+	except Exception as e:
+		print(e)
+		return f"[WARN]: Fetch failed"
+
+# type: () ->
+def load_pngs_to_numpy_list(directory):
+    image_list = {}
+    for filename in os.listdir(directory):
+        if filename.lower().endswith('.png'):
+            img_path = os.path.join(directory, filename)
+            image = Image.open(img_path).convert('RGB')  # or 'RGBA' if alpha channel is needed
+            image_array = np.array(image)
+            image_list[filename.replace('.png', '')] = image_array
+    return image_list
+
+
+### main
+
+# define query
+query = "Los Angeles Riots"
+
+# fetch text and normalise language
+query_results = get_query(query, volume=10)
+query_urls = [x['url'] for x in query_results['results']]
+query_text = list(map(url_2_txt, query_urls))
+query_text_en = list(map(normalise_language, query_text))
+
+# trace
+for t in query_text_en:
+	print(t)
+	print("=============================")
+print(f"[Elapsed time: {time.time()-T0:.3f}s]")
+
+# perform analysis
+emotion_classifier = pipeline('text-classification', model='bhadresh-savani/bert-base-uncased-emotion')
+query_text_en_emotion = list(map(lambda t: emotion_classifier(t[:512]), query_text_en))
+print(query_text_en_emotion)
+print(f"[Elapsed time: {time.time()-T0:.3f}s]")
+
+emotion_images = load_pngs_to_numpy_list('assets')
+for data, url in zip(query_text_en_emotion, query_urls):
+	source = url.split('/')[2]
+	emotion = data[0]['label']
+	score = data[0]['score']
+	plt.imshow(emotion_images[emotion], alpha=score)
+	plt.text(10, 10, f"{source}\n\"{query}\" {emotion}: {score*100:.2f}%", c='r')
+	plt.show()
+print(f"[Elapsed time: {time.time()-T0:.3f}s]")
