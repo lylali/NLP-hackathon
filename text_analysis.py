d1b62238ded7517c54fb1fc837b0410844117c29
Joseph <perryjd@cardiff.ac.uk>
2025-06-11 09:30:20 +0100

    Added BERTTopic model

--- text_analysis.py
+++ text_analysis.py
@@ -1,11 +1,20 @@
 from transformers import pipeline
+from bertopic import BERTopic
 
+# example prompt
 prompt = "Tensions rise in the South China Sea as the United States moves to target commercial transports in contested border region"
 
-classifier = pipeline('text-classification', model='bhadresh-savani/bert-base-uncased-emotion', return_all_scores=True)
-prediction = classifier(prompt)
+# emotion classifier
+emotion_classifier = pipeline('text-classification', model='bhadresh-savani/bert-base-uncased-emotion', return_all_scores=True)
+emotion_prediction = emotion_classifier(prompt)
 print(prediction)
 
-classifier = pipeline('text-classification', model='harshal-11/Bert-political-classification', return_all_scores=True)
-prediction = classifier(prompt)
+# politics classifier ###! (no class labels)
+politic_classifier = pipeline('text-classification', model='harshal-11/Bert-political-classification', return_all_scores=True)
+politic_prediction = politic_classifier(prompt)
 print(prediction)
+
+# BERT Topic classifier
+topic_classifier = BERTopic.load("MaartenGr/BERTopic_Wikipedia")
+topic, prob = topic_model.transform(prompt)
+print(topic_model.topic_labels_[topic])
